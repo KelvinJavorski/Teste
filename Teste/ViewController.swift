ba7a10c6dc6f75686c2665097a3f0e1760fcd1e9
Kelvin Javorski Soares <kelvin.javorskisoares@gmail.com>
2019-08-22 15:30:03 -0300

    teste

--- Teste/ViewController.swift
+++ Teste/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        teste = 2
+        //teste
     }
 
 
